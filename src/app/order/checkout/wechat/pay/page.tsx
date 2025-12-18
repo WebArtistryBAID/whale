a17444797a1e652751be238066658f6e567637cc
Dreta <32196824+Dreta@users.noreply.github.com>
2025-12-18 14:08:10 +0800

    fix: cannot ask friend to pay by sharing link

--- src/app/order/checkout/wechat/pay/page.tsx
+++ src/app/order/checkout/wechat/pay/page.tsx
@@ -1,7 +1,6 @@
 import { getOrder } from '@/app/lib/ordering-actions'
 import { redirect } from 'next/navigation'
 import { PaymentStatus } from '@/generated/prisma/client'
-import { getMyUser } from '@/app/login/login-actions'
 import OrderPayClient from '@/app/order/checkout/wechat/pay/OrderPayClient'
 import CookiesBoundary from '@/app/lib/CookiesBoundary'
 
@@ -9,7 +8,6 @@ export default async function OrderPayBase({ searchParams }: {
     searchParams?: Promise<{ [_: string]: string | string[] | undefined }>
 }) {
     const id = (await searchParams)!.id as string
-    const me = await getMyUser()
     const order = await getOrder(parseInt(id as string))
     if (order == null) {
         redirect('/')
